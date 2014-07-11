9906fb1c4126eeb15eb8c12cec9013dbbbc5cf4c
Kiran G <kirang20@gmail.com>
2014-07-11 17:48:28 +0530

    Kiran | Minor refactoring.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name editorDemoApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the editorDemoApp
- */
 angular.module('editorDemoApp')
   .controller('MainCtrl', function ($scope) {
     $scope.customers = ['Aspen', 'WKLI', 'OUP'];
@@ -37,7 +30,7 @@ angular.module('editorDemoApp')
 
     $scope.updateChapters = function(customer) {
         $scope.filteredChapters = $scope.chapters.filter(function(chapter){
-            return chapter.customer == customer;
+            return chapter.customer === customer;
         });
     };
 
@@ -45,40 +38,40 @@ angular.module('editorDemoApp')
     $scope.updateChapters($scope.selectedCustomer);
 
     $scope.editorOptions = {
-        language: 'en',
-        toolbar: [
-            { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
-            { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
-            { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
-            { name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
+        'language': 'en',
+        'toolbar': [
+            { 'name': 'document', 'groups': [ 'mode', 'document', 'doctools' ], 'items': [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
+            { 'name': 'clipboard', 'groups': [ 'clipboard', 'undo' ], 'items': [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+            { 'name': 'editing', 'groups': [ 'find', 'selection', 'spellchecker' ], 'items': [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+            { 'name': 'forms', 'items': [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
             '/',
-            { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
-            { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
-            { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-            { name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+            { 'name': 'basicstyles', 'groups': [ 'basicstyles', 'cleanup' ], 'items': [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+            { 'name': 'paragraph', 'groups': [ 'list', 'indent', 'blocks', 'align', 'bidi' ], 'items': [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
+            { 'name': 'links', 'items': [ 'Link', 'Unlink', 'Anchor' ] },
+            { 'name': 'insert', 'items': [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
             '/',
-            { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
-            { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
-            { name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
-            { name: 'others', items: [ '-' ] },
-            { name: 'about', items: [ 'About' ] }
+            { 'name': 'styles', 'items': [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+            { 'name': 'colors', 'items': [ 'TextColor', 'BGColor' ] },
+            { 'name': 'tools', 'items': [ 'Maximize', 'ShowBlocks' ] },
+            { 'name': 'others', 'items': [ '-' ] },
+            { 'name': 'about', 'items': [ 'About' ] }
                  ],
-        toolbarGroups : [
-            { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-            { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-            { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ] },
-            { name: 'forms' },
+        'toolbarGroups' : [
+            { 'name': 'document', 'groups': [ 'mode', 'document', 'doctools' ] },
+            { 'name': 'clipboard', 'groups': [ 'clipboard', 'undo' ] },
+            { 'name': 'editing', 'groups': [ 'find', 'selection', 'spellchecker' ] },
+            { 'name': 'forms' },
             '/',
-            { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-            { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-            { name: 'links' },
-            { name: 'insert' },
+            { 'name': 'basicstyles', 'groups': [ 'basicstyles', 'cleanup' ] },
+            { 'name': 'paragraph', 'groups': [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+            { 'name': 'links' },
+            { 'name': 'insert' },
             '/',
-            { name: 'styles' },
-            { name: 'colors' },
-            { name: 'tools' },
-            { name: 'others' },
-            { name: 'about' }
+            { 'name': 'styles' },
+            { 'name': 'colors' },
+            { 'name': 'tools' },
+            { 'name': 'others' },
+            { 'name': 'about' }
                         ]
     };
   });
