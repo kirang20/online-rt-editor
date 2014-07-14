f2131bb9230d0eb4cab22f112d87b0266c72b2d1
Kiran G <kirang20@gmail.com>
2014-07-14 17:57:28 +0530

    Kiran | Changed the deploy directory to dist.

--- Gruntfile.js
+++ Gruntfile.js
@@ -370,7 +370,7 @@ module.exports = function (grunt) {
     //Deployment
     buildcontrol: {
         options: {
-          dir: 'app',
+          dir: 'dist',
           commit: true,
           push: true,
           message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
